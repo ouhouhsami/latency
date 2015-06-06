69acd0825811cbbf09c58db4a803a44c6bdae8bc
ouhouhsami <samuel.goldszmidt@gmail.com>
2015-06-06 14:26:14 +0200

    improve ui

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var audioContext = new window.AudioContext();
 var sample = document.querySelector('#sample');
 var sampleBis = document.querySelector('#sampleBis');
 var timeIt = document.querySelector('#time');
+var playBt = document.querySelector('#play');
 var sampleMediaElementSource = audioContext.createMediaElementSource(sample);
 
 
@@ -11,44 +12,14 @@ var val3 = document.querySelector('#val3');
 var val4 = document.querySelector('#val4');
 var val5 = document.querySelector('#val5');
 
-//var bufferSource = audioContext.createBufferSource();
-
 var initTime = audioContext.currentTime;
 
 sampleMediaElementSource.connect(audioContext.destination);
 
-/*
-
-function loadSample(url){
-    return new Promise(function(resolve, reject){
-        fetch(url)
-        .then((response) => {
-            return response.arrayBuffer()
-        })
-        .then((buffer) =>{
-            audioContext.decodeAudioData(buffer, (decodedAudioData) =>{
-                resolve(decodedAudioData);
-            })
-        });
-    })
-};
-
-loadSample('vista.wav')
-.then(
-    function(buffer){
-        bufferSource.buffer = buffer;
-        bufferSource.connect(audioContext.destination);
-        //bufferSource.start();
-        initTime = audioContext.currentTime
-        sample.play();
-        sampleBis.play();
-    }
-);
-
-*/
-
-sample.play();
-sampleBis.play();
+playBt.onclick = function(evt){
+    sample.play();
+    sampleBis.play();
+}
 
 timeIt.onclick = function(evt){
     var diff = audioContext.currentTime-initTime;
